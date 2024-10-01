b0a1f5cd36068540b382403c0266d5e2b2e60fb4
SuhachevN <tradenoff@gmail.com>
2024-10-02 01:43:21 +0500

    KS-1-2

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson1
+
+fun main() {
+    val NumberOfOrders: Int = 72
+    val CongratulationText: String = "Поздравляем с обновлением!"
+    println(NumberOfOrders)
+    println(CongratulationText)
+    var NumberOfEmployees: Int = 2000
+    //println(NumberOfEmployees)
+    NumberOfEmployees = 1999
+    println(NumberOfEmployees)
+}
