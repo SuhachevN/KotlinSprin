606cf02425367d9d50bd19f329d184399e82b78b
SuhachevN <tradenoff@gmail.com>
2024-10-02 01:42:38 +0500

    Initial commit

---
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson1
+
+fun main() {
+    val NumberOfOrders: Int = 72
+    val CongratulationText: String = "Поздравляем с обновлением!"
+    println(NumberOfOrders)
+    println(CongratulationText)
+}
