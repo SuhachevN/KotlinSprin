1ef383ff75e99e4e6b1b401b5d0ee823fd253e02
SuhachevN <tradenoff@gmail.com>
2024-10-02 13:12:20 +0500

    KS-1-1-solved task1

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -3,6 +3,4 @@ package org.example.lesson1
 fun main() {
     val NumberOfOrders: Int = 72
     val CongratulationText: String = "Поздравляем с обновлением!"
-    println(NumberOfOrders)
-    println(CongratulationText)
 }
