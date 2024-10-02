5d3b350e50e3fe8ab9a6f88dc68a318ad92894d2
SuhachevN <tradenoff@gmail.com>
2024-10-02 13:23:12 +0500

    KS-1-2-solved task2

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -3,10 +3,13 @@ package org.example.lesson1
 fun main() {
     val NumberOfOrders: Int = 72
     val CongratulationText: String = "Поздравляем с обновлением!"
+
     println(NumberOfOrders)
     println(CongratulationText)
+
     var NumberOfEmployees: Int = 2000
     //println(NumberOfEmployees)
     NumberOfEmployees = 1999
+
     println(NumberOfEmployees)
 }
